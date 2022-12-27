eac3fcdcafc644169b82b94db50357452b052bd5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-12-27 13:37:39 +0530

    Add basic feature + test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [color, setColor] = useState('red');
+    const newBtnColor = color === 'red' ? 'blue': 'red'
+    const handleColorChange = () => {
+            setColor(prev => prev === 'red' ? 'blue': 'red')
+    }
+    return (
+        <div>
+            <button
+                style={{backgroundColor: color}}
+                onClick={handleColorChange}
+            >
+                Change to {newBtnColor}
+            </button>
+
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should have initial color as red', function () {
+    render(<App />)
+    const btn = screen.getByRole('button', {name : /change to blue/i})
+    expect(btn).toHaveStyle({backgroundColor: 'red'})
+});
+
+
+it('should change color to blue once clicked', function () {
+    render(<App />)
+    const btn = screen.getByRole('button', {name: /change to blue/i})
+    fireEvent.click(btn)
+    expect(btn).toHaveStyle({backgroundColor: 'blue'})
 });
